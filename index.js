aa3c1209f71d63493f32dea57f07de8cbdcd7ea1
Rahul M R <rahulmr@gmail.com>
2018-03-13 18:12:29 +0530

    a

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
-const http = require('http')
-const https = require('https');
-function handler(req,res){
+'use strict';
 
-    res.end('Hello World')
+// returns an instance of node-greenlock with additional helper methods
+var lex = require('greenlock-express').create({
+  // set to https://acme-v01.api.letsencrypt.org/directory in production
+  server: 'staging'
 
-}
+// If you wish to replace the default plugins, you may do so here
+//
+, challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) }
+, store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' })
 
-http.createServer(handler).listen(80)
+// You probably wouldn't need to replace the default sni handler
+// See https://git.daplie.com/Daplie/le-sni-auto if you think you do
+//, sni: require('le-sni-auto').create({})
 
-const PROD = false;
-const lex = require('greenlock-express').create({
-  server: PROD ? 'https://acme-v01.api.letsencrypt.org/directory' : 'staging',
- 
-  approveDomains: (opts, certs, cb) => {
-    if (certs) {
-      // change domain list here
-      opts.domains = ['rahulmr.win']
-    } else { 
-      // change default email to accept agreement
-      opts.email = 'rahulmr@gmail.com'; 
-      opts.agreeTos = true;
-    }
-    cb(null, { options: opts, certs: certs });
-  }
+, approveDomains: approveDomains
 });
-const middlewareWrapper = lex.middleware;
+function approveDomains(opts, certs, cb) {
+  // This is where you check your database and associated
+  // email addresses with domains and agreements and such
 
-https.createServer(
-  lex.httpsOptions, 
-  middlewareWrapper(handler)
-).listen(433);
+
+  // The domains being approved for the first time are listed in opts.domains
+  // Certs being renewed are listed in certs.altnames
+  if (certs) {
+    opts.domains =['rahulmr.win','www.rahulmr.win']
+  }
+  else {
+    opts.email = 'john.doe@example.com';
+    opts.agreeTos = true;
+  }
+
+  // NOTE: you can also change other options such as `challengeType` and `challenge`
+  // opts.challengeType = 'http-01';
+  // opts.challenge = require('le-challenge-fs').create({});
+
+  cb(null, { options: opts, certs: certs });
+}
+// handles acme-challenge and redirects to https
+require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
+  console.log("Listening for ACME http-01 challenges on", this.address());
+});
+
+
+
+var app = require('express')();
+app.use('/', function (req, res) {
+  res.end('Hello, World!');
+});
+
+// handles your app
+require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(443, function () {
+  console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+});
